57e0ab3c04ede6a83696655a5bf91e0a7542c3d2
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-13 21:13:18 -0600

    he IaC for crate a Lambda Function and Api to expose the

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,50 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LambdaIntegration, RestApi, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime, Code, Function } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import { Cors } from 'aws-cdk-lib/aws-apigateway';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * 
+     */
+    const TRPCLambdaFunction = new Function(this, 'trpc-lambda-handler', {
+      runtime: Runtime.NODEJS_22_X,
+      handler: 'server.handler',
+      code: Code.fromAsset(join(__dirname, '../../backend/production/')),
+      memorySize: 128,
+      timeout: cdk.Duration.minutes(1),
+      environment: {
+        MODE:  'production',
+        MONGODB_URI_DEV: process.env.MONGODB_URI_DEV ?? '',
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    /**
+     * 
+     */
+    const api = new LambdaRestApi(this, 'flower-shop-api-trpc', {
+      handler: TRPCLambdaFunction,
+      proxy: true,
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['localhost'],
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: Cors.DEFAULT_HEADERS,
+      },
+    });
+
+
+    /**
+     * 
+     */
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+    });
   }
 }
