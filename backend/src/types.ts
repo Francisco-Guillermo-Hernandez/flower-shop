da1900c83172a29ba6b51c9f986e1eda3d2e710d
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-16 09:01:51 -0600

    Add types for the validator

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 import { FlowerValidator } from './validators/flower.validator';
 import { DeleteValidator } from './validators/delete.validator';
+import { OderValidatorValidator } from './validators/order.validator';
 import { TypeOf, z, ZodType } from 'zod';
 import { $ZodTypeInternals } from 'zod/v4/core/schemas.cjs';
 import { APIGatewayProxyEventHeaders } from 'aws-lambda/trigger/api-gateway-proxy';
@@ -15,6 +16,7 @@ export type Params<TT extends z.ZodTypeAny> = {
     },  
 };
 
+export type OderTypeValidator = typeof OderValidatorValidator;
 export type FlowerTypeValidator = typeof FlowerValidator;
 export type DeleteValidatorType = typeof DeleteValidator;
 export type TRPCResponse = {
