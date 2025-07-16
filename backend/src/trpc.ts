2880233d87458e7ad5528d7d7c97b66d605d2256
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-15 23:56:10 -0600

    Add Context to the TRPC server and add some new middlewares

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,15 +1,46 @@
-// import 'reflect-metadata';
+
 import { TRPCError, initTRPC } from '@trpc/server';
+import { Context, AnotherContext } from './context'
+import { Responses as R } from './utils/responses';
 import { dataSource } from './db';
-import { MoreThan } from 'typeorm'
+import { ZodError } from 'zod';
+import env from './environment'
 
-
-export interface ProcedureMeta {
+type ProcedureMeta = {
     authenticated?: boolean;
 }
 
-const server = initTRPC.meta<ProcedureMeta>().create({
-    defaultMeta: { authenticated: true }
+const server = initTRPC
+.context<AnotherContext | Context>()
+.meta<ProcedureMeta>()
+.create({
+    defaultMeta: { authenticated: true },
+    errorFormatter: ({ shape, error }) => {
+    try {
+      return {
+        ...shape,
+        message: JSON.parse(shape?.message ?? '{}'),
+        data: {
+          ...{
+            ...shape.data,
+            stack: env.MODE === 'developmen t' ? shape.data.stack : ''
+          },
+          zodError: error.code === 'BAD_REQUEST' && error.cause instanceof ZodError ? error.cause.flatten() : null,
+        },
+      };
+    } catch (e: any) {
+       return {
+        ...shape,
+        message: shape.message ?? '',
+        data: {
+          ...{
+            ...shape.data,
+            stack: env.MODE === 'developmen t' ? shape.data.stack : ''
+          }
+        },
+      };
+    }
+  },
 });
 
 
@@ -18,8 +49,15 @@ const server = initTRPC.meta<ProcedureMeta>().create({
  * 
  */
 const dataSourceMiddleware = server.middleware(async ({ ctx, next }) => await next({ ctx: { ...ctx, dataSource: dataSource() }}));
+const authedProcedure = server.middleware(async ({ meta, next, ctx }) => {
+  if (meta?.authenticated) {
+    throw new TRPCError({ code: R.Unauthorized, cause: '', message: 'You are not authorized to do anything on this server' });
+  }
+  return next();
+});
+
+const passIncomingHeadersMiddleware = server.middleware(async ({ ctx, next }) => next({ ctx: {...ctx, headers: ctx?.headers ?? []} }));
 
 export const router = server.router;
-export const procedure = server.procedure.use(dataSourceMiddleware);
+export const procedure = server.procedure.use(dataSourceMiddleware).use(authedProcedure).use(passIncomingHeadersMiddleware);
 export const mergeRouters = server.mergeRouters;
-
