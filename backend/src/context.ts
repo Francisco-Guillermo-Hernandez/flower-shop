4361ce7124a39612fb1ae3ffc9d8cba81d656f50
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-16 00:00:27 -0600

    Add headers to the context

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -1,18 +1,32 @@
 import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
+import { CreateHTTPContextOptions } from '@trpc/server/adapters/standalone';
 import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 
+type CreateContextOptions = { session: { user: string } | null; };
+
+/**
+ * 
+ * @param param
+ * @returns 
+ */
 export const createContext = ({ event, context }: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({
     event: event,
     apiVersion: (event as {version?: string}).version ?? '1.0',
     user: event.headers['x-user'],
+    session: { user: { userName: '' } },
+    headers: event?.headers ?? []
 });
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
 
-export const createLocalContext = ({event, context }: any ) => ({
-    event: event,
-    user: event.headers['x-user'],
-    ...context
-})
 
-export type LocalContext = Awaited<ReturnType<typeof createLocalContext>>;
+/**
+ * 
+ * @param param
+ * @returns 
+ */
+export const createAnotherContext = async ({ req, res }: CreateHTTPContextOptions) => ({
+    headers: req?.headers ?? []
+});
+
+export type AnotherContext = Awaited<ReturnType<typeof createAnotherContext>>;
