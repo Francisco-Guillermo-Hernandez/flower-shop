39e190d5dc85717ee1f9109be4020d586c7308eb
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-16 00:04:41 -0600

    Implement the methods into the router

--- backend/src/routers/flower.router.ts
+++ backend/src/routers/flower.router.ts
@@ -1,10 +1,18 @@
 
 import { router, procedure } from '../trpc'
-import { listAllFlowers } from '../controllers/flower.controller';
+import { FlowerController } from '../controllers/flower.controller';
+import { FlowerValidator } from '../validators/flower.validator';
+import { DeleteValidator } from '../validators/delete.validator';
+
+const flowerController = new FlowerController();
 
 /**
  * 
  */
 export const FlowerRouter = router({
-    list: procedure.meta({ authenticated: false }).query(({ctx}) => listAllFlowers({ctx})),
+    list: procedure.meta({ authenticated: true }).query(({ctx}) => flowerController.listAll({ctx})),
+    findById: procedure.meta({ authenticated: false }).query(({ctx}) => flowerController.findById({ctx})),
+    create: procedure.meta({ authenticated: false }).input(FlowerValidator).mutation(({ctx, input}) => flowerController.create({ctx, input})),
+    update: procedure.meta({ authenticated: false }).input(FlowerValidator).mutation(({ctx, input}) => flowerController.update({ctx, input})),
+    delete: procedure.meta({ authenticated: false }).input(DeleteValidator).mutation(({ctx, input}) => flowerController.delete({ctx, input})),
 });
