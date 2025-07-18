14e1a1c14d6ef655fab6517bab0cfa92042ebe64
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-07-18 01:53:27 -0600

    Improve validator

--- backend/src/validators/flower.validator.ts
+++ backend/src/validators/flower.validator.ts
@@ -3,17 +3,16 @@ import { z } from 'zod';
 const allowedCharsRegex = /^[A-Za-z0-9 ]+$/;
 
 export const FlowerValidator = z.object({
-    id: z.string().optional(),
 
     name: z.string().regex(allowedCharsRegex, { message: 'String must contain only letters and numbers' }).max(50, { message: '' }).min(5, { message: '' }),
 
-    description: z.string(),
+    description: z.string().min(5),
     
-    existences: z.number(),
+    existences: z.number().min(10, { message: 'Too small' }),
     
-    pricePerUnit: z.number(),
+    pricePerUnit: z.number().min(0.99, { message: 'Too small' }),
 
-    costPerUnit: z.number(),
+    costPerUnit: z.number().min(0.10, { message: 'Too small' }),
     
     image: z.string().optional(),
 });
